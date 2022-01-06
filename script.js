9cb52b2194ba77008d49a40b205499cf1eb722ec
AndMe <andrevieira@lme.ufc.br>
2022-01-05 22:37:58 -0300

    Inicio das funções do script

---
+++ script.js
@@ -0,0 +1,59 @@
+let order = [];
+let clickerOrder = [];
+let score = 0;
+
+/* 
+0 - verde
+1 - vermelho
+2 - amarelo
+3 - azul 
+*/
+
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+
+// função para sortear números entre 0 e 3
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickerOrder = [];
+
+    for (let i in order) {
+        let elementColor = creatColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1)
+    }
+}
+
+// função para acender as cores
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected')
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected')
+    });
+}
+
+// comparação da ordem gerada e clicada
+let checkOrder = () => {
+    for (let i in clickerOrder) {
+        if (clickerOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+
+    if (clickerOrder.length == order.length) {
+        alert(`Pontuação: ${score} \n Você acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+// função para clique do usuário
+let click = (color) => {
+    clickerOrder[clickerOrder.length] = color;
+    elementColor(color).classList.add('selected')
+}
